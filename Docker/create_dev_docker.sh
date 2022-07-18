53edf261223c7c01530c8be34286f36d71a940b8
khalidzhang <khalidzhang@tencent.com>
2022-07-18 11:07:59 +0800

    refactor proj

--- Docker/create_dev_docker.sh
+++
@@ -1,4 +0,0 @@
- # build docker
- docker build -t clion/ubuntu_dev -f Dockefile.default .
- # create
- docker run -d --cap-add sys_ptrace -p 127.0.0.1:2233:22 -v /g/projects:/data/projects --name clion_ubuntu clion/ubuntu_dev
